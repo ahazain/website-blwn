9d5ed0c8cbb7da7584359d9cd2f537381b8237cc
ahazain <ahmadfariddd5@gmail.com>
2024-09-29 12:46:10 +0700

    ejs

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const Queries = require("./connectQuery"); //koneksi pg
 const app = express();
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.render("home");
 });
 
 app.get("/users", Queries.getUsers);
