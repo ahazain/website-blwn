69f4b974e495f1587da4315d36d3ae250288d0b1
ahazain <ahmadfariddd5@gmail.com>
2024-09-29 12:05:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
